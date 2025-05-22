c8c2db87c12fec7092758af06c48027a90d4caa4
andimags <andimagsumbol@gmail.com>
2025-05-22 17:53:49 +0800

    update checkPermission middleware to check only the permissions from global-scope roles

--- src/middlewares/checkPermission.ts
+++ src/middlewares/checkPermission.ts
@@ -1,56 +1,46 @@
 // middleware/authorize.ts
 import { NextFunction, Request, Response } from 'express';
 import { Op } from 'sequelize';
-import Channel from '../database/models/Channel';
 import User from '../database/models/User';
 import { IAuthenticatedRequest } from '../types';
 import { AppError } from './errorHandler';
 
-export default function checkPermission(permissionRefName: string, channelRefName: string) {
+// Middleware for auth service, checks permission on GLOBAL scope
+export default function checkPermission(permissionRefName: string | string[]) {
     return async (req: Request, res: Response, next: NextFunction): Promise<any> => {
         try {
-            // 1. Check if x-api-key is match to the channel with ref_name from channelRefName variable
-            // 2. If x-api-key is GLOBAL, check user's roles with channel_id as NULL
-            // 3. If x-api-key has value, check user's roles with channel_id that has value
-            // 4. Loop through the roles and check if it has permission with ref_name from permissionRefName variable
             const apiKey = req.header('x-api-key');
             const decoded = (req as IAuthenticatedRequest).user;
             const user = await User.findByPk(decoded.id);
 
-            let roles = null;
             let isAuthorized = false;
 
             if (!apiKey) throw new AppError('API Key not found', 401);
 
-            if(channelRefName == 'GLOBAL' && apiKey == 'GLOBAL'){
-                roles = await user?.getRoles({where: { channel_id: {[Op.is]: null}}});
-                if (!roles) throw new AppError('User has no global roles assigned', 401);
-            }
-            else{
-                const channel = await Channel.findOne({where: {api_key: apiKey}});
-                if (!channel) throw new AppError('Invalid API Key', 401);
-                roles = await user?.getRoles({where: {channel_id: channel.id}});
-                if (!roles) throw new AppError(`User has no roles assigned for ${channel.name} channel`, 401);
-            }
+            const roles = await user?.getRoles({where: { channel_id: {[Op.is]: null}}});
+            if (!roles) throw new AppError('User has no global roles assigned', 401);
 
-                    for (const role of roles) {
-            const permissions = await role.getPermissions();
+            for (const role of roles) {
+                const whereCondition = Array.isArray(permissionRefName)
+                    ? { ref_name: { [Op.in]: permissionRefName } }
+                    : { ref_name: permissionRefName };
 
-            for (const permission of permissions) {
-                if (permission.ref_name === permissionRefName) {
+                const [permission] = await role.getPermissions({
+                    where: whereCondition,
+                    limit: 1,
+                });
+
+                if (permission) {
                     isAuthorized = true;
                     break;
                 }
             }
 
-            if (isAuthorized) break;
-        }
-
-        if (isAuthorized) {
-            next();
-        } else {
-            throw new AppError('Unauthorized', 401);
-        }
+            if (isAuthorized) {
+                next();
+            } else {
+                throw new AppError('Unauthorized', 401);
+            }
         } catch (error: unknown) {
             next(error);
         }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
-import checkPermission from '../middlewares/checkPermission';
 import { authMiddleware } from '../middlewares/authMiddleware';
+import checkPermission from '../middlewares/checkPermission';
 
 const userRoutes = Router();
 
-userRoutes.get('/', authMiddleware, checkPermission('view:user', 'GLOBAL') , userController.getAll);
+userRoutes.get('/', authMiddleware, checkPermission(['view:user', 'admin:user']) , userController.getAll);
 userRoutes.get('/:id', userController.find);
 userRoutes.post('/', userController.add);
 userRoutes.put('/:id', userController.update);
