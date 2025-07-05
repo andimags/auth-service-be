6e1c959fd7879b7380eb9d15d74304e1450217db
andimags <andimagsumbol@gmail.com>
2025-07-05 14:15:32 +0800

    create validators for user role routes

--- src/routes/userRoleRoutes.ts
+++ src/routes/userRoleRoutes.ts
@@ -1,29 +1,38 @@
 import { Router } from 'express';
 import userRoleController from '../controllers/userRoleController';
 import checkPermission from '../middlewares/checkPermission';
+import { validationMiddleware } from '../middlewares/validationMiddleware';
+import { addValidator } from '../validators/user-role/addValidator';
+import { deleteValidator } from '../validators/user-role/deleteValidator';
+import { getValidator } from '../validators/user-role/getValidator';
+import { replaceValidator } from '../validators/user-role/replaceValidator';
 
 const userRoleRoutes = Router();
 
 userRoleRoutes.get(
     '/user/:user_id',
     checkPermission(['view:user_role', 'admin:user_role'], false),
+    validationMiddleware(getValidator),
     userRoleController.getUserRoles
 );
 
 userRoleRoutes.post(
     '/user/:user_id',
     checkPermission(['assign:user_role', 'admin:user_role'], false),
+    validationMiddleware(addValidator),
     userRoleController.addUserRoles
 );
 
 userRoleRoutes.put(
     '/user/:user_id',
     checkPermission(['update:user_role', 'admin:user_role'], false),
+    validationMiddleware(replaceValidator),
     userRoleController.replaceUserRoles
 );
 userRoleRoutes.delete(
     '/user/:user_id',
     checkPermission(['remove:user_role', 'admin:user_role'], false),
+    validationMiddleware(deleteValidator),
     userRoleController.destroyUserRole
 );
 
---
+++ src/validators/user-role/deleteValidator.ts
@@ -0,0 +1,26 @@
+import { body, param } from 'express-validator';
+
+export const deleteValidator = [
+    param('user_id')
+        .notEmpty()
+        .withMessage('User ID is required')
+        .bail()
+        .isInt()
+        .withMessage('User ID must be integer'),
+
+    body('role_ids')
+        .notEmpty()
+        .withMessage('Role IDs are required')
+        .bail()
+        .custom((value) => {
+            // If it's a single value, convert it to an array
+            const values = Array.isArray(value) ? value : [value];
+
+            // Validate all are integers
+            if (!values.every((v) => Number.isInteger(Number(v)))) {
+                throw new Error('All role IDs must be integers');
+            }
+
+            return true;
+        })
+];
