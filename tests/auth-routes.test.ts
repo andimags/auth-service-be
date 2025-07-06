ca73db2fa1b2694a1f189bc8e1b67172d6f94751
andimags <andimagsumbol@gmail.com>
2025-07-06 16:35:55 +0800

    test: add test cases for has-any-permission endpoint of auth routes

--- tests/auth-routes.test.ts
+++ tests/auth-routes.test.ts
@@ -2,20 +2,26 @@ import request from 'supertest';
 import app from '../src/app';
 import User from '../src/database/models/User';
 import sequelize from '../src/database/sequelize';
-import { forceDeleteInstances, generateUserData } from './utils';
+import { createAuthHeaders, createAuthUser, createRole, forceDeleteInstances, generateUserData } from './utils';
 
 describe('Auth Routes', () => {
+    interface IAuth {
+        token: string | null;
+        user: User | null;
+    }
+
     let token: string;
     let authUser: User | null;
 
     const AGENT = request.agent(app); // preserves cookies
     const DEFAULT_PASSWORD = 'abcd1234';
+    const API_BASE_URL = '/api/auth';
 
     beforeAll(async () => {
         await sequelize.sync(); // or authenticate() if DB is already ready
         authUser = await User.create(generateUserData());
 
-        const res = await AGENT.post('/api/auth/generate-token').send({
+        const res = await AGENT.post(`${API_BASE_URL}/generate-token`).send({
             email: authUser.email,
             password: DEFAULT_PASSWORD
         });
@@ -31,7 +37,7 @@ describe('Auth Routes', () => {
     describe('POST /api/auth/generate-token', () => {
         it('should return 200 with valid credentials', async () => {
             const response = await request(app)
-                .post('/api/auth/generate-token')
+                .post(`${API_BASE_URL}/generate-token`)
                 .send({
                     email: 'superadmin@gmail.com',
                     password: 'abcd1234'
@@ -46,7 +52,7 @@ describe('Auth Routes', () => {
 
         it('should return 401 with invalid credentials (user not existing)', async () => {
             await request(app)
-                .post('/api/auth/generate-token')
+                .post(`${API_BASE_URL}/generate-token`)
                 .send({
                     email: 'superadmin123@gmail.com',
                     password: 'abcd12345'
@@ -60,7 +66,7 @@ describe('Auth Routes', () => {
 
         it('should return 401 with invalid credentials (incorrect email or password)', async () => {
             await request(app)
-                .post('/api/auth/generate-token')
+                .post(`${API_BASE_URL}/generate-token`)
                 .send({
                     email: 'superadmin@gmail.com',
                     password: 'abcd12345'
@@ -76,7 +82,7 @@ describe('Auth Routes', () => {
     describe('GET /api/auth/verify-token', () => {
         it('should return 200 with valid token', async () => {
             const response = await request(app)
-                .get('/api/auth/verify-token')
+                .get(`${API_BASE_URL}/verify-token`)
                 .set('Authorization', `Bearer ${token}`)
                 .expect('Content-Type', /json/)
                 .expect(200);
@@ -88,7 +94,7 @@ describe('Auth Routes', () => {
 
         it('should return 403 with invalid token', async () => {
             const response = await request(app)
-                .get('/api/auth/verify-token')
+                .get(`${API_BASE_URL}/verify-token`)
                 .set('Authorization', `Bearer ${token}xxx`)
                 .expect('Content-Type', /json/)
                 .expect(401);
@@ -101,7 +107,7 @@ describe('Auth Routes', () => {
 
     describe('GET /api/auth/refresh-token', () => {
         it('should return 200 with refreshed token', async () => {
-            const response = await AGENT.get('/api/auth/refresh-token')
+            const response = await AGENT.get(`${API_BASE_URL}/refresh-token`)
                 .expect('Content-Type', /json/)
                 .expect(200);
 
@@ -112,11 +118,59 @@ describe('Auth Routes', () => {
 
         it('should return 403 refreshed token', async () => {
             const response = await request(app)
-                .get('/api/auth/refresh-token')
+                .get(`${API_BASE_URL}/refresh-token`)
                 .expect('Content-Type', /json/)
                 .expect(403);
 
             expect(response.body).toEqual({ message: 'Token not found' });
+        });
+    });
+
+    describe('GET /api/auth/has-any-permission', () => {
+        it('should return 200 with refreshed token', async () => {
+            const customAuthUser: IAuth = await createAuthUser();
+            const customAuthUserRole = await createRole(['view:user']);
+            await customAuthUser.user?.setRoles(customAuthUserRole);
+
+            const payload = {
+                permission_ref_names: "view:user",
+                permission_scope: "global"
+            }
+            
+            const response = await request(app)
+                .get(`${API_BASE_URL}/has-any-permission`)
+                .set(createAuthHeaders(customAuthUser.token!))
+                .send(payload)
+                .expect('Content-Type', /json/)
+                .expect(200);
+
+            expect(response.body).toMatchObject({
+                status: 1
+            });
+
+            await forceDeleteInstances([customAuthUser.user!])
+        });
+
+        it('should return 403 when authorized user has no permissions or permissions does not exist', async () => {
+            const customAuthUser: IAuth = await createAuthUser();
+
+            const payload = {
+                permission_ref_names: "view:user",
+                permission_scope: "global"
+            }
+            
+            const response = await request(app)
+                .get(`${API_BASE_URL}/has-any-permission`)
+                .set(createAuthHeaders(customAuthUser.token!))
+                .send(payload)
+                .expect('Content-Type', /json/)
+                .expect(403);
+
+            expect(response.body).toMatchObject({
+                message: "Unauthorized"
+            });
+
+            await forceDeleteInstances([customAuthUser.user!])
         });
     });
 });
