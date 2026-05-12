1296847c43b4c4b8a5e696364e7098a322d0dcb3
andimags <andimagsumbol@gmail.com>
2026-05-12 14:23:56 +0800

    feat: remove auto assign role on superadmin user upon creation

--- src/database/models/Role.ts
+++ src/database/models/Role.ts
@@ -133,13 +133,6 @@ export default class Role extends Model {
     >;
 
     // Hooks
-    @AfterCreate
-    static async assignToSuperadminUser(role: Role) {
-        const user = await User.findOne({ where: { username: 'superadmin' } });
-
-        await user?.addRoles([role]);
-    }
-
     @BeforeCreate
     static preventAddingAnotherSuperadminRole(instance: Role) {
         if (
