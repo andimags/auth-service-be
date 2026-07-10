2e8da4e3580727ab9bebb82196fc0d37fd1daab7
andimags <andimagsumbol@gmail.com>
2026-07-10 22:27:54 +0800

    chore(be): resolve stale jest.config.ts TODO comments
    The silent/verbose flags had inline TODO comments never acted on, and
    the test:verbose npm script set verbose=false, contradicting its own
    name. Kept default npm test output as-is (silent, non-verbose) and
    fixed test:verbose to actually pass verbose=true.

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    "silent": true,  // Remove this or set to false
-    "verbose": false // Remove this or set to true
+    silent: true,
+    verbose: false
 };
